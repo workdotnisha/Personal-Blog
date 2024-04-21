0eae269aa4673ef06e53f6fa8a3f9c1c16ee3cd7
workdotnisha <workdotnisha@gmail.com>
2024-04-21 20:12:44 +0530

    modifying html js and css files

--- js/script.js
+++ js/script.js
@@ -4,11 +4,16 @@ const addEventOnElements = function(elements,eventType,callback){
         elements[i].addEventListener(eventType,callback);
     }
 }
+/**
+ * MOBILE NAVBAR TOGGLER
+ */
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 
-const toggleNav = () => navbar.classList.toggle("active");
-
+const toggleNav = () => {
+    navbar.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
 addEventOnElements(navTogglers, "click", toggleNav); 
 
 const header = document.querySelector("[data-header]");
@@ -19,4 +24,62 @@ window.addEventListener("scroll", () => {
  }else{
     header.classList.remove("active");
  }
+});
+
+/**
+ * SLIDER
+ */
+const slider = document.querySelector("[data-slider]");
+const sliderContainer = document.querySelector("[data-slider-container]");
+const sliderPrevBtn = document.querySelector("[data-slider-prev]");
+const sliderNextBtn = document.querySelector("[data-slider-next]");
+
+let totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue
+("--slider-items"));
+let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+let currentSlidePos = 0;
+
+const moveSliderItem = function(){
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+}
+/**
+ * NEXT SLIDER
+ */
+const slideNext = function(){
+    const slideEnd = currentSlidePos >= totalSlidableItems;
+    if(slideEnd){
+        currentSlidePos = 0;
+    }else{
+        currentSlidePos++;
+    }
+    moveSliderItem();
+}
+
+sliderNextBtn.addEventListener("click", slideNext);
+
+/**
+ * PREVIOUS SLIDE
+ */
+const slidePrev = function(){
+    const slideEnd = currentSlidePos <= 0;
+    if(slideEnd){
+        currentSlidePos = totalSlidableItems - 1;
+    }else{
+        currentSlidePos--;
+    }
+    moveSliderItem();
+}
+
+sliderPrevBtn.addEventListener("click", slidePrev);
+
+/**
+ * RESPONSIVE
+ */
+window.addEventListener("resize", function(){
+    totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue
+    ("--slider-items"));
+    totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    moveSliderItem();
 });
